345ddcdef343e84e0cb528d755d2b71a87a78cf6
nikohuber <nrh9757@rit.edu>
2023-09-06 22:51:57 -0400

    variablesAndExpressions

---
+++ VariablesAndExpressions/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Niko Huber
+// IGME 201
+// Generates product of four user inputs
+
+namespace VariablesAndExpressions // Main namespace
+{
+    internal class Program
+    {
+        static void Main(string[] args) // main
+        {
+
+            // array to store user variables and product
+            int[] numArray = new int[4];
+            int product = 0;
+
+            for (int i = 0; i < 4; i++)  // looping through one readNum function for each unit in array
+            {
+                numArray[i] = readNum();
+            }
+
+            product = numArray[0] * numArray[1] * numArray[2] * numArray[3]; // calculating product
+
+            Console.WriteLine("The product of your numbers is: " + product); // final console message
+        }
+        
+        static int readNum() // readNum function for reading user input
+        {
+
+            // temp variables
+            String tempNum = null;
+            int? inputNum = null;
+
+            Console.WriteLine("Please enter an integer."); // intro message
+
+            while(inputNum == null) // while loop continues until a proper number  is acquired
+            {
+                tempNum = Console.ReadLine();
+
+                try // attempts a conversion
+                {
+                    inputNum = Convert.ToInt32(tempNum);
+                }
+                catch // catch statement
+                {
+                    Console.WriteLine("Please enter an INTEGER.");
+                }
+            }
+           
+            return inputNum.Value ; // returns the value of the nullified int
+        }
+    }
+}
---
+++ VariablesAndExpressions/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VariablesAndExpressions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VariablesAndExpressions")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bb71150b-00c8-46e4-a108-0586dc16866b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
