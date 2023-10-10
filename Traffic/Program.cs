dccdecb8fc492f0fc44251a2f9c84af17de20f10
nikohuber <nrh9757@rit.edu>
2023-10-10 19:24:48 -0400

    pe11 - traffic

---
+++ Traffic/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicles;
+
+
+// Niko Huber
+// IGME 201
+// Traffic
+namespace Traffic
+{
+
+    // traffic
+    internal class Program
+    {
+        // main (empty)
+        static void Main(string[] args)
+        {
+
+        }
+
+        // add passenger- of passengerCarrier interface 
+        void AddPassenger(PassengerCarrier passenger)
+        {
+
+            // loads passenger
+            passenger.LoadPassenger();
+
+            // writes string of passenger
+            Console.WriteLine(passenger.ToString());
+        }
+    }
+}
