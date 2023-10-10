49f57f16b15392c9ca4556134ef9260d030aadb9
nikohuber <nrh9757@rit.edu>
2023-10-10 19:27:54 -0400

    pe11 - vehicles (commented)

--- Vehicles/Class1.cs
+++ Vehicles/Class1.cs
@@ -4,37 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Niko Huber
+// IGME 201
+// Vehicles
 namespace Vehicles
 {
+    // Abstract Vehicle Class
     public abstract class Vehicle
     {
+
+        // LoadPassenger
         public virtual void  LoadPassenger()
         {
 
         }
     }
 
+    // PassengerCarrier interface
     public interface PassengerCarrier
     {
          void  LoadPassenger();
 
     }
 
+    // HeavyLoadCarrier interface
     public interface HeavyLoadCarrier
     {
 
     }
 
+    // Car
     public abstract class Car
     {
 
     }
 
+    // Train
     public abstract class Train
     {
 
     }
 
+    // Compact Car
     public class Compact : Car, PassengerCarrier
     {
         public virtual void LoadPassenger()
@@ -44,6 +55,7 @@ namespace Vehicles
 
     }
 
+    // SUV Car
     public class SUV: Car, PassengerCarrier
     {
         public virtual void LoadPassenger()
@@ -53,11 +65,13 @@ namespace Vehicles
 
     }
 
+    // Pickup Car
     public class Pickup : Car, HeavyLoadCarrier
     {
 
     }
 
+    // Passenger Train
     public class PassengerTrain: Train, PassengerCarrier
     {
         public virtual void LoadPassenger()
@@ -67,11 +81,13 @@ namespace Vehicles
 
     }
 
+    // Freight Train
     public class FreightTrain: Train, HeavyLoadCarrier
     {
 
     }
 
+    // DoubleBogey Train
     public class DoubleBogey: Train, HeavyLoadCarrier
     {
 
