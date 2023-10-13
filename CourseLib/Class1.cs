ddb9ddc0d81a0a9d73c156c18f7d336f1ac2167a
nikohuber <nrh9757@rit.edu>
2023-10-13 14:59:20 -0400

    CourseLib

---
+++ CourseLib/Class1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseLib
+{
+    public class Schedule
+    {
+
+        public DateTime startTime;
+        public DateTime endTime;
+        public List<DayOfWeek> daysOfWeek = new List<DayOfWeek>();
+
+    }
+
+    public class Courses
+    {
+
+        //+this.courseCode
+
+        public Course this[string courseCode]
+        {
+            get
+            {
+                Course returnVal;
+                try
+                {
+                    returnVal = (Course)sortedList[courseCode];
+                }
+                catch
+                {
+                    returnVal = null;
+                }
+
+                return returnVal;
+            }
+            set
+            {
+                try
+                {
+                    sortedList[courseCode] = value;
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        SortedList<string, Course> sortedList;
+
+        public Courses()
+        {
+            Course thisCourse;
+            Schedule thisSchedule;
+
+            Random rand = new Random();
+
+            // generate courses IGME-200 through IGME-299
+            for (int i = 200; i < 300; ++i)
+            {
+                // use constructor to create new course object with code and description
+                thisCourse = new Course(($"IGME-{i}"), ($"Description for IGME-{i}"));
+
+                // create a new Schedule object
+                thisSchedule = new Schedule();
+                for (int dow = 0; dow < 7; ++dow)
+                {
+                    // 50% chance of the class being on this day of week
+                    if (rand.Next(0, 2) == 1)
+                    {
+                        // add to the daysOfWeek list
+                        thisSchedule.daysOfWeek.Add((DayOfWeek)dow);
+
+                        // select random hour of day
+                        int nHour = rand.Next(0, 24);
+
+                        // set start and end times of minute duration
+                        // select fixed date to allow time calculations
+                        thisSchedule.startTime = new DateTime(1, 1, 1, nHour, 0, 0);
+                        thisSchedule.endTime = new DateTime(1, 1, 1, nHour, 50, 0);
+                    }
+                }
+
+                // set the schedule for this course
+                thisCourse.schedule = thisSchedule;
+
+                // add this course to the SortedList
+                this[thisCourse.courseCode] = thisCourse;
+            }
+        }
+
+        // public courseCode this{get; set; }
+
+        public void Remove(string courseCode)
+        {
+
+            if (courseCode != null)
+            {
+                sortedList.Remove(courseCode);
+            }
+
+        }
+
+    }
+
+    public class Course
+    {
+        public Course(string courseCode, string description)
+        {
+
+        }
+        public string courseCode { get; set; }
+
+        public string description { get; set; }
+
+        public string teacherEmail { get; set; }
+
+        public Schedule schedule { get; set; }
+
+
+    }
+
+
+}
