56d87bd26a4e30912a1c61a5ead924828192b787
nikohuber <nrh9757@rit.edu>
2023-10-29 17:31:10 -0400

    question 14

---
+++ exam2/q14/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+// Niko Huber
+// Exam 2
+// IGME 201
+namespace StructToClass
+{
+    // friend class
+    public class Friend : ICloneable
+    {
+        public string name;
+        public string greeting;
+        public DateTime birthdate;
+        public string address;
+
+        // clone!
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+
+    // main
+    class Program
+    {
+
+        // main cont
+        static void Main(string[] args)
+        {
+            // create new class objects
+            Friend friend = new Friend();
+            Friend enemy = new Friend();
+
+            // create my friend Charlie Sheen
+            friend.name = "Charlie Sheen";
+            friend.greeting = "Dear Charlie";
+            friend.birthdate = DateTime.Parse("1967-12-25");
+            friend.address = "123 Any Street, NY NY 12202";
+
+            // now he has become my enemy
+            // clone friend
+            enemy = (Friend)friend.Clone();
+
+            // set the enemy greeting and address without changing the friend variable
+            enemy.greeting = "Sorry Charlie";
+            enemy.address = "Return to sender.  Address unknown.";
+
+            Console.WriteLine($"friend.greeting => enemy.greeting: {friend.greeting} => {enemy.greeting}");
+            Console.WriteLine($"friend.address => enemy.address: {friend.address} => {enemy.address}");
+        }
+    }
+}
---
+++ exam2/question4-7/Program.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question4_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Tardis tardis = new Tardis();
+            PhoneBooth phoneBooth = new PhoneBooth();
+
+            UsePhone(tardis);
+            UsePhone(phoneBooth);
+            
+
+        }
+
+
+        static void UsePhone(object obj)
+        {
+
+            PhoneInterface phoneInterface = (PhoneInterface)obj;
+            phoneInterface.MakeCall();
+            phoneInterface.HangUp();
+
+            if(phoneInterface.GetType() == typeof(PhoneBooth))
+            {
+                PhoneBooth phoneBooth = (PhoneBooth)phoneInterface;
+                phoneBooth.OpenDoor();
+                phoneBooth.CloseDoor();
+            }
+        }
+
+        public class Tardis : RotaryPhone
+        {
+            private bool sonicScrewdriver;
+            private byte whichDrWho;
+            private string femaleSideKick;
+            public double exteriorSurfaceArea;
+            public double interiorVolume;
+
+            public static bool operator ==(Tardis tardis1, Tardis tardis2)
+            {
+
+                return (tardis1 == tardis2);
+            }
+            public static bool operator !=(Tardis tardis1, Tardis tardis2)
+            {
+                return (tardis1 != tardis2);
+            }
+
+            public static bool operator >=(Tardis tardis1, Tardis tardis2)
+            {
+                if(tardis1.whichDrWho == 10)
+                {
+                    return true;
+                }
+                else
+                {
+                    return (tardis1 >= tardis2);
+                }
+            }
+            public static bool operator <=(Tardis tardis1, Tardis tardis2)
+            {
+                return (tardis1 <= tardis2);
+            }
+
+            public static bool operator >(Tardis tardis1, Tardis tardis2)
+            {
+                if (tardis1.whichDrWho == 10)
+                {
+                    return true;
+                }
+                else
+                {
+                    return (tardis1 > tardis2);
+                }
+            }
+            public static bool operator <(Tardis tardis1, Tardis tardis2)
+            {
+                return (tardis1 > tardis2);
+            }
+
+            public byte WhichDrWho
+            {
+                get
+                {
+                    return WhichDrWho;
+                }
+                set
+                {
+                    whichDrWho = value;
+                }
+            }
+
+            public string FemaleSideKick
+            {
+                get
+                {
+                    return FemaleSideKick;
+                }
+            }
+
+            public void TimeTravel()
+            {
+
+            }
+        }
+
+        public class RotaryPhone : Phone, PhoneInterface
+        {
+            public void Answer()
+            {
+
+            }
+            public void MakeCall()
+            {
+
+            }
+            public void HangUp()
+            {
+
+            }
+
+            public override void Connect()
+            {
+
+            }
+
+            public override void Disconnect()
+            {
+
+            }
+
+        }
+
+        public abstract class Phone
+        {
+            private string phoneNumber;
+            public string address;
+            public string PhoneNumber;
+
+            public abstract void Connect();
+            public abstract void Disconnect();
+        }
+
+        public interface PhoneInterface
+        {
+            void Answer();
+            void MakeCall();
+            void HangUp();
+
+        }
+
+        public class PushButtonPhone : Phone, PhoneInterface
+        {
+            public void Answer()
+            {
+
+            }
+            public void MakeCall()
+            {
+
+            }
+            public void HangUp()
+            {
+
+            }
+            public override void Connect()
+            {
+
+            }
+            public override void Disconnect()
+            {
+
+            }
+        }
+
+        public class PhoneBooth : PushButtonPhone
+        {
+            private bool superMan;
+            public double costPerCall;
+            public bool phoneBook;
+
+            public void OpenDoor()
+            {
+
+            }
+            public void CloseDoor()
+            {
+
+            }
+        }
+    }
+}
